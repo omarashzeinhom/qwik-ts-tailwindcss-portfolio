61282151cf094b2165603e9fa7601c76ed7423b8
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-12-21 13:30:40 +0200

    ADDED Portofolio GUI

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -20,7 +20,8 @@ export default component$(() => {
 
               return (
                 <>
-                  <div class="w-1/2 p-1 md:p-2">
+
+                  <div class="w-1/2 p-1 md:p-2 rounded-lg shadow-lg bg-white max-w-sm">
                     <img
                       key={index}
                       src={item?.img}
@@ -28,7 +29,11 @@ export default component$(() => {
                       alt="Camera"
                     />
                   </div>
-
+                  <div class="p-6">
+                    <p class="text-gray-700 text-base mb-4">
+                      {item?.title}
+                    </p>
+                  </div>
                 </>
               )
 
@@ -47,25 +52,24 @@ export default component$(() => {
 
 export const portfolioItemsObject = [
   {
-    title: "",
+    title: "Project A",
     img: "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1653641536/personal_portofolio_assets_2022/certificateitems/udemy_python_cert_item_4_catkfr.webp",
     link: "",
   },
   {
-    title: "",
+    title: "Project B",
     img: "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1653641536/personal_portofolio_assets_2022/certificateitems/udemy_python_cert_item_4_catkfr.webp",
     link: "",
   },
   {
-    title: "",
+    title: "Project C",
     img: "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1653641530/personal_portofolio_assets_2022/certificateitems/udacity_nanodegree_cert_item_1_yrf9nh.webp",
     link: "",
   },
   {
-    title: "",
+    title: "Project D",
     img: "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1653641530/personal_portofolio_assets_2022/certificateitems/cybrary_cert_item_3_ixkele.webp",
     link: "",
   },
 ];
-
 //console.log(portfolioItemsObject);
